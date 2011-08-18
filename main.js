f90e45b60d486e740d4d4089f9e3f8236cf997ef
Nash Lincoln <nashira_lincoln@yahoo.com>
2011-08-17 19:47:18 -0700

    organize

--- main.js
+++ main.js
@@ -2,9 +2,9 @@ var repl = require('repl'),
     context = repl.start('>').context,
     fs = require('fs');
 
-var metaGrammar = context.metaGrammar = fs.readFileSync('./meta.meta');
-var ParseError  = context.ParseError = require('./parse_error.js').ParseError;
-var Meta        = context.Meta = require('./meta.js').Meta;
-var MetaParser  = context.MetaParser = require('./meta_parser.js').MetaParser;
+var metaGrammar = context.metaGrammar = fs.readFileSync('./lang/meta.meta');
+var ParseError  = context.ParseError = require('./lib/parse_error.js').ParseError;
+var Meta        = context.Meta = require('./lib/meta.js').Meta;
+var MetaParser  = context.MetaParser = require('./lib/meta_parser.js').MetaParser;
 
 context.mp = new MetaParser(metaGrammar.toString());
